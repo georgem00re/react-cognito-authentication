ff67c17c5a7fd744dddd9bbd736cd9dcbde237b8
george <george.moore@cyted.ai>
2025-07-20 13:36:05 +0100

    update Terraform to deploy a very basic Cognito user pool

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,14 +17,16 @@ terraform {
   }
 }
 
+locals {
+  cognito_user_pool_name = "MyExampleCognitoUserPool"
+}
+
 provider "aws" {
   region  = var.AWS_REGION
   profile = var.AWS_PROFILE
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "MyExampleVPC"
-  }
+module "aws_cognito_user_pool" {
+  source = "./modules/aws_cognito_user_pool"
+  name   = local.cognito_user_pool_name
 }
